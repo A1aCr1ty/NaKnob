66b908b44d386bd14ebfad8cde37d84db478c823
Nanisha430 <542297096@qq.com>
2023-12-07 09:57:58 +0800

    添加 dialpad和switches ui

--- src/app/resources/ResourcePool.cpp
+++ src/app/resources/ResourcePool.cpp
@@ -3,18 +3,21 @@
 /* Global resource manager */
 ResourcePool Resource;
 
-extern "C" {
-#define IMPORT_FONT(name) \
-do{\
-    LV_FONT_DECLARE(font_##name)\
-    Resource.Font_.AddResource(#name, (void*)&font_##name);\
-}while(0)
+extern "C"
+{
+#define IMPORT_FONT(name)                                        \
+    do                                                           \
+    {                                                            \
+        LV_FONT_DECLARE(font_##name)                             \
+        Resource.Font_.AddResource(#name, (void *)&font_##name); \
+    } while (0)
 
-#define IMPORT_IMG(name) \
-do{\
-    LV_IMG_DECLARE(img_src_##name)\
-    Resource.Image_.AddResource(#name, (void*)&img_src_##name);\
-}while (0)
+#define IMPORT_IMG(name)                                             \
+    do                                                               \
+    {                                                                \
+        LV_IMG_DECLARE(img_src_##name)                               \
+        Resource.Image_.AddResource(#name, (void *)&img_src_##name); \
+    } while (0)
 
     static void Resource_Init()
     {
@@ -52,8 +55,8 @@ do{\
 
         // IMPORT_IMG(arm);
         IMPORT_IMG(bluetooth);
-        IMPORT_IMG(pose6d);
-        IMPORT_IMG(joints);
+        IMPORT_IMG(switches);
+        IMPORT_IMG(dialpad);
         IMPORT_IMG(miku);
     }
 
@@ -64,7 +67,7 @@ void ResourcePool::Init()
     lv_obj_remove_style_all(lv_scr_act());
     lv_disp_set_bg_color(lv_disp_get_default(), lv_color_black());
 
-    Font_.SetDefault((void*)&lv_font_montserrat_14);
+    Font_.SetDefault((void *)&lv_font_montserrat_14);
 
     Resource_Init();
 }
